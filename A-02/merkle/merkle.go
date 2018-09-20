40d3adde68ae7e6de904d796bc35a3ba9e0fc487
Shawn Enriques <42825178+senriques@users.noreply.github.com>
2018-09-20 09:18:36 -0600

    Add files via upload

---
+++ A-02/merkle/merkle.go
@@ -0,0 +1,18 @@
+// Shawn Enriques
+// senriqu1@uwyo.edu
+// Assignment 3 Merkle Trees
+
+package merkle
+
+import (
+	"fmt"
+
+//	"github.com/Univ-Wyo-Education/Blockchain-4010-Fall-2018/Assignments/A-03/hash"
+)
+
+func MerkleHash()		{
+
+	fmt.Printf("-MerkleHash\n")
+	return
+//	return InstructorMerkleHash(data)
+}
